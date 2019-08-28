0a094b2d1137fed3aab7e35f12751b228fa71495
Alex Odorico <alex.odorico@morleynet.com>
2019-08-28 12:08:50 -0400

    Seperate handlers for GET and POST requests

--- main.go
+++ main.go
@@ -22,6 +22,15 @@ func main() {
 }
 
 func handleUser(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getUser(w, r)
+	case "POST":
+		postUser(w, r)
+	default:
+		fmt.Printf("Only GET and POST methods are allowed on this endpoint")
+	}
+
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		dbuser, dbpassword, dbname)
 	db, err := sql.Open("postgres", dbinfo)
@@ -39,6 +48,14 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 
 	}
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("GET on /api/user")
+}
+
+func postUser(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("POST on /api/user")
 }
 
 func checkErr(err error) {
