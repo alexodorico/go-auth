fec07533433bf126c51c7c6e188f42a8519d30b4
Alex Odorico <alex.odorico@morleynet.com>
2019-08-28 13:19:44 -0400

    Have GET and POST working for /api/users

--- main.go
+++ main.go
@@ -22,40 +22,44 @@ func main() {
 }
 
 func handleUser(w http.ResponseWriter, r *http.Request) {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
+		dbuser, dbpassword, dbname)
+	db, err := sql.Open("postgres", dbinfo)
+
 	switch r.Method {
 	case "GET":
-		getUser(w, r)
+		getUser(w, r, db)
 	case "POST":
-		postUser(w, r)
+		postUser(w, r, db)
 	default:
 		fmt.Printf("Only GET and POST methods are allowed on this endpoint")
 	}
 
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		dbuser, dbpassword, dbname)
-	db, err := sql.Open("postgres", dbinfo)
 	checkErr(err)
 	defer db.Close()
+}
 
-	var lastInsertID int
-	err = db.QueryRow("INSERT INTO users(username,password) VALUES($1,$2) RETURNING id", "oxideorcoal", "wow").Scan(&lastInsertID)
-	checkErr(err)
-	fmt.Println("last inserted ID =", lastInsertID)
-
-	rows, err := db.Query("SELECT * FROM users")
+func getUser(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	fmt.Printf("GET on /api/user")
+	rows, err := db.Query("SELECT id, username, password FROM users")
 	checkErr(err)
 
 	for rows.Next() {
-
+		var id int
+		var username string
+		var password string
+		err = rows.Scan(&id, &username, &password)
+		checkErr(err)
+		fmt.Printf("%3v | %s | %s ", id, username, password)
 	}
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("GET on /api/user")
-}
-
-func postUser(w http.ResponseWriter, r *http.Request) {
+func postUser(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	fmt.Printf("POST on /api/user")
+	var lastInsertID int
+	err := db.QueryRow("INSERT INTO users(username,password) VALUES($1,$2) RETURNING id", "oxideorcoal", "wow").Scan(&lastInsertID)
+	checkErr(err)
+	fmt.Println("last inserted ID =", lastInsertID)
 }
 
 func checkErr(err error) {
