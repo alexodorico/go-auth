5fd3450f29c783a6132ab75c97485fdae27a3137
Alex Odorico <alex.odorico@morleynet.com>
2019-08-28 14:38:00 -0400

    Returning JSON data of user to client

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -16,10 +17,10 @@ var (
 	dbname     = os.Getenv("DB_NAME")
 )
 
-type User struct {
-	id       int
-	username string
-	password string
+type user struct {
+	UID      int    `json:"uid"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func main() {
@@ -47,6 +48,7 @@ func handleUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func getUser(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	var u user
 	params := strings.Split(r.URL.Path, "/")
 	uid := params[3]
 	rows, err := db.Query("SELECT id, username, password FROM users WHERE id = $1", uid)
@@ -54,12 +56,18 @@ func getUser(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 
 	for rows.Next() {
 		var id int
-		var username string
-		var password string
-		err = rows.Scan(&id, &username, &password)
+		var uname string
+		var pw string
+		err = rows.Scan(&id, &uname, &pw)
+		u = user{UID: id, Username: uname, Password: pw}
 		checkErr(err)
-		fmt.Printf("%v | %v | %v", id, username, password)
 	}
+
+	j, err := json.Marshal(u)
+	checkErr(err)
+	fmt.Println(string(j))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(j)
 }
 
 func postUser(w http.ResponseWriter, r *http.Request, db *sql.DB) {
